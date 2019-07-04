c458e96b81d66b3522c06e8f8a449ed07ec10069
accentureXRS <marco.tempest@accenture.com>
2019-07-04 13:59:30 +0200

    ios docs

--- ios_version.js
+++ ios_version.js
@@ -2,4 +2,8 @@ var iosDownloads = [
     makeResourceLink("","","7/1/19")
 ];
 
+var iosDocs = [
+    makeResourceLink("https:\/\/www.dropbox.com/s/s4fdfpo8bihysh9/Tech_Vision_XR_2019_Setup_Guide.pdf?dl=0", "Setup and Operation Guide", "7/1/19");
+];
+
 var manifestUrl = "https:\/\/extendedrealitystudio.github.io/TVXR19/manifest.plist";
