3dcb1b79736700653604c2e9023267e9d0f5cf48
accentureXRS <marco.tempest@accenture.com>
2019-07-04 14:04:53 +0200

    other typo

--- ios_version.js
+++ ios_version.js
@@ -3,7 +3,7 @@ var iosDownloads = [
 ];
 
 var iosDocs = [
-    makeResourceLink("https:\/\/www.dropbox.com/s/s4fdfpo8bihysh9/Tech_Vision_XR_2019_Setup_Guide.pdf?dl=0", "Setup and Operation Guide", "7/1/19");
+    makeResourceLink("https:\/\/www.dropbox.com/s/s4fdfpo8bihysh9/Tech_Vision_XR_2019_Setup_Guide.pdf?dl=0", "Setup and Operation Guide", "7/1/19")
 ];
 
 var manifestUrl = "https:\/\/extendedrealitystudio.github.io/TVXR19/manifest.plist";
