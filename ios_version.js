af392f770ba3f5206c6b660c47215ad7e2063e31
ExtendedRealityStudio <37864346+ExtendedRealityStudio@users.noreply.github.com>
2019-08-22 17:31:29 +0200

    Update ios_version.js

--- ios_version.js
+++ ios_version.js
@@ -4,7 +4,8 @@ var iosDownloads = [
 
 var iosDocs = [
     //makeResourceLink("https:\/\/ts.accenture.com/sites/NanoLabs/Files/Forms/AllItems.aspx?id=%2Fsites%2FFiles%2FDemos%2FNano%20Labs%20Demos%2FTech%20Vision%20XR%202019", "Setup and Operation Guide", "22/8/19")
-    makeResourceLink("https:\/\/www.dropbox.com/s/s4fdfpo8bihysh9/Tech_Vision_XR_2019_Setup_Guide.pdf?dl=0", "Setup and Operation Guide", "8/22/19")
+    //makeResourceLink("https:\/\/www.dropbox.com/s/s4fdfpo8bihysh9/Tech_Vision_XR_2019_Setup_Guide.pdf?dl=0", "Setup and Operation Guide", "8/22/19")
+    makeResourceLink("https:\/\/www.dropbox.com/sh/2h8hzuv7354cv0c/AABkY94pn_MmvLtO31_DRacQa?dl=0", "Setup and Operation Guide", "8/22/19")
 ];
 
 var manifestUrl = "https:\/\/extendedrealitystudio.github.io/TVXR19/manifest.plist";
