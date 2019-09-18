dd6b59e0d05cb6d9a0edd929c51586c9a50c46de
AccentureXRS <>
2019-09-18 10:03:57 +0200

    new robot links

--- robot_version.js
+++ robot_version.js
@@ -3,7 +3,7 @@ var robotDownloads = [
     makeResourceLink("https:\/\/www.dropbox.com/s/iclrsmyjz36atye/app-release.apk?dl=1", "the robot app", "7/1/19"),
     makeResourceLink("https:\/\/www.dropbox.com/s/11f690hwm88ti6y/RobotPlaylistEditor.zip?dl=1", "the robot playlist", "7/1/19"),
     makeResourceLink("https:\/\/www.dropbox.com/s/g9u8uq476rif0wr/TVXR19_robot_AR.zip?dl=1", "the AR app", "7/1/19"),
-    makeResourceLink("https:\/\/www.dropbox.com/s/b2xgcw9mjvet4zz/TVXR19_remote.zip?dl=1", "the remote controller", "7/1/19")
+    makeResourceLink("https:\/\/www.dropbox.com/s/b2xgcw9mjvet4zz/TVXR19_remote.zip?dl=0", "the remote controller", "7/1/19")
 ];
 
 var robotDocs = [
